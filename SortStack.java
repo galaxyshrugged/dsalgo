5b49120c1f0ea51848d073ab0ee2dc690dfb349b
Nikhil Katre <nkatre@users.noreply.github.com>
2014-01-13 20:05:48 -0500

    Update SortStack.java
    A stack is given which supports the following methods. Push, pop, peek and isEmpty. Sort this stack irrespective of its inner structure.
    Solution
    
    We will recursively pop the stack till it is empty, Then we will try to put the popped number with another recursive function named insertSorted. This will push to the stack if the top of the stack is greater than the number, if it is not, then it will pop the number and call the insertSorted with the popped number and the rest of the stack. After it returns it will push the number in correct place.

--- SortStack.java
+++ SortStack.java
@@ -1,3 +1,13 @@
+A stack is given which supports the following methods. Push, pop, peek and isEmpty. 
+Sort this stack irrespective of its inner structure.
+Solution
+
+We will recursively pop the stack till it is empty, Then we will try to put the popped number with 
+another recursive function named insertSorted. This will push to the stack if the top of the stack is
+greater than the number, if it is not, then it will pop the number and call the insertSorted with the
+popped number and the rest of the stack. After it returns it will push the number in correct place.
+
+
 /*
 For problem and solution description please visit the link below
 http://www.dsalgo.com/2013/02/sort-stack.html
