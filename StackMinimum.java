ab1042e85bd395f291c66052defd8c1aa13a8474
Nikhil Katre <nkatre@users.noreply.github.com>
2014-01-13 20:04:29 -0500

    Update StackMinimum.java
    Problem
    
    Implement a stack which provides an efficient get minimum function along with regular push and pop functionality.
    Brute force
    
    In normal implementation of the stack with array or linked list the push and pop has O(1) complexity, But the brute force implementation of get minimum will result in a O(n) complexity.
    Solution
    
    We can implement a O(1) complexity for push, pop and get minimum by using O(n) extra space. In this solution we keep another stack for minimum calculation. Whenever a new number is inserted if it is smaller than the stack top it is inserted into this special stack otherwise the minimum is again inserted into the special stack. When get minimum is called, top of minimum stack is returned. When a number is popped from the stack, one number is popped from the min stack also. This can be done in other way also, if the number being inserted is bigger than the min stack top, nothing is done on the min stack. when a number is popped min stack is popped only when its top is equal to the number. But the former one is easy to code and both has same complexity.

--- StackMinimum.java
+++ StackMinimum.java
@@ -1,3 +1,21 @@
+Problem
+
+Implement a stack which provides an efficient get minimum function along with 
+regular push and pop functionality.
+Brute force
+
+In normal implementation of the stack with array or linked list the push and pop has
+O(1) complexity, But the brute force implementation of get minimum will result in a O(n) complexity.
+Solution
+
+We can implement a O(1) complexity for push, pop and get minimum by using O(n) extra space. 
+In this solution we keep another stack for minimum calculation. Whenever a new number is inserted 
+if it is smaller than the stack top it is inserted into this special stack otherwise the minimum is
+again inserted into the special stack. When get minimum is called, top of minimum stack is returned.
+When a number is popped from the stack, one number is popped from the min stack also. This can be done
+in other way also, if the number being inserted is bigger than the min stack top, nothing is done on
+the min stack. when a number is popped min stack is popped only when its top is equal to the number.
+But the former one is easy to code and both has same complexity.
 /*
 For problem and solution description please visit the link below
 http://www.dsalgo.com/2013/02/StackMinimum.php.html
